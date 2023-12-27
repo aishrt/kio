a2a37ae13f04e1cd5c5fa5e3eb8d7290b3d2eb22
Developer <you@example.com>
2023-12-27 17:43:13 +0530

    Image upload done

--- routes/landing.route.js
+++ routes/landing.route.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const router = express.Router();
 const { landingController } = require("../controllers");
+const upload = require("../middlewares/multer");
 
 router.get("/", landingController.landing);
+router.post("/upload", upload.single("file"), landingController.upload);
 
 module.exports = router;
